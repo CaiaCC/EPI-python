74e93f3fc8dd061c8dacb28e4528c423a786f5a6
Caia Chuang <caiachuang@gmail.com>
2024-04-01 12:49:56 -0700

    improved dutch_flag_partition

--- 02-array/dutch_flag_partition.py
+++ 02-array/dutch_flag_partition.py
@@ -1,9 +1,10 @@
 '''
+2 pass, 2 pointers
 Time: O(n)
 Space: O(1)
-'''
 
-def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+
+def dutch_flag_partition(pivot_index: int, A: list[int]) -> None:
     pivot = A[pivot_index]
     # first pass: group elements smaller than pivot
     smaller = 0
@@ -13,12 +14,37 @@ def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
             smaller += 1
     # second pass: group elements larger than pivot
     larger = len(A) - 1
-    for i in reversed(len(A)):
+    for i in reversed(range(len(A))):
         # Stop when reach an element smaller than pivot, since the first pass moved them to the start of the array
         if A[i] < pivot:
             break
         else:
             A[i], A[larger] = A[larger], A[i]
             larger -= 1
+    print(A)
+'''
 
-    return
+'''
+1 pass, 2 pointers
+Time: O(n)
+Space: O(1)
+'''
+
+def dutch_flag_partition(pivot_index: int, A: list[int]) -> None:
+    pivot = A[pivot_index]
+    smaller, current, larger = 0, 0, len(A)
+    while current < larger:
+        if A[current] < pivot:
+            A[current], A[smaller] = A[smaller], A[current]
+            smaller += 1
+            current += 1
+        elif A[current] == pivot:
+            current += 1
+        else:
+            larger -= 1
+            A[current], A[larger] = A[larger], A[current]
+
+    print(A)
+
+
+print(dutch_flag_partition(4, [-2, 0, -1, 1, 1, 3, 4, 5, 2]))
